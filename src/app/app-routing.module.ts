1f40d3e3a416c02ef36cc98cb36ae8ac47b1fb2b
Nguyen Hung <billy7d95@gmail.com>
2020-11-25 15:28:40 +0700

    done validate edit-playlist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { EditPlaylistComponent } from './components/playlist/edit-playlist/edit-playlist.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'users',pathMatch:'full'},
+  // {path:'playlists',component:ListUserComponent},
+  // {path:'playlists/details/:id',component:DetailedUserComponent},
+  // {path:'playlists/create',component:CreateUserComponent},
+  {path:'playlists/edit/:id',component:EditPlaylistComponent},
+  // {path:'playlists/view/:id',component:DetailedUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
