866d10335fe592a7b05fc2ec52dce76a10b8fde2
Nguyen Hung <billy7d95@gmail.com>
2020-11-26 21:20:55 +0700

    create song

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/song-create/song-create.component.ts
+++ src/app/components/song-create/song-create.component.ts
@@ -12,6 +12,13 @@ export class SongCreateComponent implements OnInit {
   constructor(private httpClient: HttpClient) {}
 
   songs: any = [];
+  song:any={
+    name:'',
+    album:'',
+    author:'',
+    musicType:'',
+    linkMp3:""
+  }
 
   selectedFile: File;
   retrievedImage: any;
@@ -20,45 +27,67 @@ export class SongCreateComponent implements OnInit {
   message: string;
   imageName: any;
 
-  //Gets called when the user selects an image
-  public onFileChanged(event) {
-    //Select File
-    this.selectedFile = event.target.files[0];
-  }
+
 
   ngOnInit(): void {
     this.getAllImg();
   }
 
-  //Gets called when the user clicks on submit to upload the image
-  onUpload() {
-    console.log(this.selectedFile);
+  getAllImg() {
+    this.httpClient
+      .get('http://localhost:8080/songs')
+      .subscribe((res) => {
 
+        this.songs= res;
+      });
+  }
+
+
+
+  //Gets called when the user clicks on submit to upload the image
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
     //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
     const uploadImageData = new FormData();
     uploadImageData.append(
       'imageFile',
       this.selectedFile,
-      this.selectedFile.name
-    );
+      this.selectedFile.name,
 
-    //Make a call to the Spring Boot Application to save the image
-    this.httpClient.post('http://localhost:8080/songs/create', uploadImageData, {
-        observe: 'response',
+    );
+     //Make a call to the Spring Boot Application to save the image
+    this.httpClient.post('http://localhost:8080/songs/uploadMp3', uploadImageData, {
+        observe: 'response'
       })
       .subscribe((response) => {
         if (response.status === 200) {
-          this.getAllImg();
+
+debugger
+
+
         } else {
+
         }
       });
   }
 
-  getAllImg() {
+  getLastSongOfList() {
     this.httpClient
-      .get('http://localhost:8080/songs/getFile')
+      .get('http://localhost:8080/songs/getlastsong')
       .subscribe((res) => {
-        this.songs = res;
+        debugger
+        this.song = res;
       });
+  }
+
+
+  createSong(){
+    this.httpClient
+    .post('http://localhost:8080/songs/create',this.song)
+    .subscribe((res) => {
+      debugger
+      this.songs = res;
+      this.getAllImg();
+    });
   }
 }
