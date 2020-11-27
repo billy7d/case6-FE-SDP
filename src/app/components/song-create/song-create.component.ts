347837d8ccde6c69ce2a8e5af11e4da4c402f89d
Nguyen Hung <billy7d95@gmail.com>
2020-11-27 14:09:32 +0700

    upload & song info

--- src/app/components/song-create/song-create.component.ts
+++ src/app/components/song-create/song-create.component.ts
@@ -12,14 +12,16 @@ export class SongCreateComponent implements OnInit {
   constructor(private httpClient: HttpClient) {}
 
   songs: any = [];
+  singers:any=[];
   song: any = {
     name: '',
     album: '',
     author: '',
     creator:'',
+    singerValues:[],
     musicType: '',
     description:'',
-    link: '',
+    linkMp3: '',
   };
 
   selectedFile: File;
@@ -33,7 +35,11 @@ export class SongCreateComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllSong();
+    this.getAllSinger();
   }
+
+
+
 
   getAllSong() {
     this.httpClient.get('http://localhost:8080/songs').subscribe((res) => {
@@ -41,31 +47,37 @@ export class SongCreateComponent implements OnInit {
     });
   }
 
-  //Gets called when the user clicks on submit to upload the image
+  getAllSinger() {
+    this.httpClient.get('http://localhost:8080/singers').subscribe((res) => {
+      this.singers = res;
+    });
+  }
+
+
   onFileChanged(event) {
     this.selectedFile = event.target.files[0];
-    //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+
     const uploadImageData = new FormData();
     uploadImageData.append(
       'imageFile',
       this.selectedFile,
       this.selectedFile.name
     );
-    //Make a call to the Spring Boot Application to save the image
+
     this.httpClient
-      .post('http://localhost:8080/songs/uploadmp3', uploadImageData, {
-        observe: 'response',
+      .post('http://localhost:8080/songs/uploadmp3', uploadImageData, {  observe: 'response',
+      responseType: "json"
       })
       .subscribe((res) => {
         if (res.status === 200) {
-          debugger;
-          this.song.link = res.body.linkMp3;
+          this.song.linkMp3 = res.body.linkMp3;
           this.showButton = false;
         }
       }),((err) =>{});
   }
 
   createSong() {
+    debugger
     this.httpClient
       .post('http://localhost:8080/songs/create', this.song)
       .subscribe((res) => {
